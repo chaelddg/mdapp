d76b2453590eee27a62b9f2a984e728d225a8729
chaelddg <chael.ddg@gmail.com>
2017-05-19 14:29:08 +0800

    uncomment git ignore prod buld

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const path = require('path');
 const app = express();
 const PORT = 3030;
 
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-	console.log(path.join(__dirname, '/build/index.html'));
-	res.sendFile(path.join(__dirname, '/build/index.html'));
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
