e8db0331a2667d33f3e761cec3ab675e40926850
chaelddg <chael.ddg@gmail.com>
2017-05-19 14:33:02 +0800

    server ip

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:9000/api';
+// axios.defaults.baseURL = 'http://localhost:9000/api';
+axios.defaults.baseURL = 'http://139.59.229.95:9000/api';
 export default axios;
